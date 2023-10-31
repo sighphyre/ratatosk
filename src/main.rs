7f390731b7a3fc47e4bad0e8b8c4bc24f2dfb1db
sighphyre <liquidwicked64@gmail.com>
2023-10-31 23:13:26 +0200

    wip: still very hack but build an execution tree from fragments of the parse result, should allow us to walk this with contexts

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,136 @@
 extern crate clap;
 
-use std::io::{self, Read};
-use unleash_types::client_features::ClientFeatures;
-use unleash_yggdrasil::{Context, EngineState};
+use std::{
+    collections::HashMap,
+    io::{self, Read},
+};
+use unleash_types::client_features::{
+    ClientFeature, ClientFeatures, Constraint, Segment, Strategy,
+};
+use unleash_yggdrasil::{
+    state::EnrichedContext,
+    strategy_parsing::compile_rule,
+    strategy_upgrade::{upgrade, upgrade_constraint, upgrade_strategy},
+    Context,
+};
+
+#[derive(Debug)]
+enum FragmentType {
+    Toggle,
+    Strategy,
+    Constraint,
+}
+
+#[derive(Debug)]
+struct TraversalResult {
+    fragment_type: FragmentType,
+    rule: String,
+}
+
+enum ChainType {
+    None,
+    Or,
+    And,
+}
+
+struct ExecutionNode {
+    rule: String,
+    children: Vec<ExecutionNode>,
+    chaining: ChainType,
+}
+
+fn constraint_node(constraint: &Constraint) -> ExecutionNode {
+    let rule = upgrade_constraint(constraint);
+    ExecutionNode {
+        rule: "constraint".into(),
+        children: vec![],
+        chaining: ChainType::None,
+    }
+}
+
+fn strategy_node(strategy: &Strategy, segment_map: &HashMap<i32, Segment>) -> ExecutionNode {
+    let rule = upgrade_strategy(strategy, segment_map);
+
+    let children = strategy
+        .constraints
+        .clone()
+        .unwrap_or_default()
+        .iter()
+        .map(constraint_node)
+        .collect();
+
+    ExecutionNode {
+        rule,
+        children,
+        chaining: ChainType::And,
+    }
+}
+
+fn toggle_node(feature: &ClientFeature, segment_map: &HashMap<i32, Segment>) -> ExecutionNode {
+    let rule = if feature.enabled {
+        "true".into()
+    } else {
+        "false".into()
+    };
+
+    let children: Vec<ExecutionNode> = feature
+        .strategies
+        .clone()
+        .unwrap_or_default()
+        .iter()
+        .map(|strategy| strategy_node(strategy, segment_map))
+        .collect();
+
+    ExecutionNode {
+        rule,
+        children,
+        chaining: ChainType::Or,
+    }
+}
+
+fn build_execution_tree(
+    feature: &ClientFeature,
+    segment_map: &HashMap<i32, Segment>,
+) -> Vec<TraversalResult> {
+    let node = toggle_node(feature, segment_map);
+
+    let mut traversal_items: Vec<TraversalResult> = vec![];
+
+    if feature.enabled {
+        traversal_items.push(TraversalResult {
+            rule: "true".into(),
+            fragment_type: FragmentType::Toggle,
+        });
+    } else {
+        traversal_items.push(TraversalResult {
+            rule: "false".into(),
+            fragment_type: FragmentType::Toggle,
+        });
+    }
+
+    let base_context = Context::default();
+    let enriched_context = EnrichedContext::from(base_context, feature.name.clone());
+
+    for strategy in feature.strategies.clone().unwrap_or_default() {
+        let strategy_without_constraints = Strategy {
+            constraints: None,
+            ..strategy.clone()
+        };
+
+        traversal_items.push(TraversalResult {
+            rule: upgrade_strategy(&strategy_without_constraints, &segment_map),
+            fragment_type: FragmentType::Strategy,
+        });
+
+        for constraint in strategy.constraints.unwrap_or_default() {}
+    }
+
+    traversal_items
+
+    // traversal_items.append(other)
+}
+
+// fn build_execution_tree(feature: Option<&ClientFeature>, segment_map: &HashMap<i32, Segment>) {}
 
 fn main() {
     let mut input = String::new();
@@ -10,14 +138,37 @@ fn main() {
     io::stdin()
         .read_to_string(&mut input)
         .expect("Failed to read from stdin");
-    let toggles =
+    let features =
         serde_json::from_str::<ClientFeatures>(&input).expect("Failed to parse input as JSON");
 
-    let mut engine = EngineState::default();
-    engine.take_state(toggles);
+    let feature = features
+        .features
+        .iter()
+        .find(|f| f.name == "Feature.A")
+        .expect("Failed to find feature by name")
+        .clone();
 
-    println!(
-        "Wheee {:?}",
-        engine.is_enabled("Feature.A", &Context::default())
-    );
+    let segment_map: HashMap<i32, Segment> = features
+        .segments
+        .unwrap_or_default()
+        .iter()
+        .map(|segment| (segment.id, segment.clone()))
+        .collect();
+
+    let explanation = build_execution_tree(&feature, &segment_map);
+
+    println!("Explanation: {:#?}", explanation);
+
+    let t = ExecutionNode {
+        rule: "true".into(),
+        children: vec![ExecutionNode {
+            rule: "false".into(),
+            children: vec![],
+            chaining: ChainType::None,
+        }],
+        chaining: ChainType::None,
+    };
+
+    // upgrade(&feature.strategies.unwrap_or(vec![]), &segment_map);
+    // println!("WHEEE")
 }

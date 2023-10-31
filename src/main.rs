e4cd3e4794cf59e278b33fb842650d18ab133de9
sighphyre <liquidwicked64@gmail.com>
2023-10-31 21:25:04 +0200

    read toggles from command line and print enabled status

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+
+use std::io::{self, Read};
+use unleash_types::client_features::ClientFeatures;
+use unleash_yggdrasil::{Context, EngineState};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read from stdin");
+    let toggles =
+        serde_json::from_str::<ClientFeatures>(&input).expect("Failed to parse input as JSON");
+
+    let mut engine = EngineState::default();
+    engine.take_state(toggles);
+
+    println!(
+        "Wheee {:?}",
+        engine.is_enabled("Feature.A", &Context::default())
+    );
 }
